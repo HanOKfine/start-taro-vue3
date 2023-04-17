07c8faa18f969ba42a00f914270377b21e47b01a
tfs_yujh <987643667@qq.com>
2023-04-17 09:57:52 +0800

    替换成setup 语法糖

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,32 +1,21 @@
 <template>
   <view class="index">
     <text>{{ msg }}</text>
+    <text>{{ cameraBack }}</text>
     <camera 
-    device-position="back"
     flash="off" 
-    :device-position="device?'front':'back'"
+    :device-position="cameraBack?'front':'back'"
     binderror="error" 
-    style="width: 100%;
-    height: 300px;"></camera>
-    <button @click="chooseImage">按钮</button>
+    style="width: 100%;height: 300px;"></camera>
+    <button @tap="toggleCameraBack">按钮</button>
   </view>
 </template>
 
-<script>
+<script lang="ts" setup>
 import { ref } from 'vue'
 import { useBoolean } from '@/hooks';
 import './index.scss'
+const { bool: cameraBack, toggle: toggleCameraBack } = useBoolean(false);
+const msg = ref('Hello world 1')
 
-export default {
-  setup () {
-    const { bool: cameraBack, setBool: setCameraBack, toggle: toggleCameraBack } = useBoolean(true);
-    const msg = ref('Hello world 1')
-
-    return {
-      msg,
-      cameraBack,
-      chooseImage
-    }
-  }
-}
 </script>
